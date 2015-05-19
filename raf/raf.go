d43ec21498c4f4f817e222bb14ca6d1b1556e077
Archs <gitarchs@yeah.net>
2015-05-19 20:08:42 +0800

    add raf to repos

---
+++ raf/raf.go
@@ -0,0 +1,53 @@
+// Copyright 2015 Joseph Hager. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build js
+
+package raf
+
+import (
+	"math"
+
+	"github.com/gopherjs/gopherjs/js"
+)
+
+func init() {
+	window := js.Global
+	vendors := []string{"ms", "moz", "webkit", "o"}
+	if window.Get("requestAnimationFrame") == js.Undefined {
+		for i := 0; i < len(vendors) && window.Get("requestAnimationFrame") == js.Undefined; i++ {
+			vendor := vendors[i]
+			window.Set("requestAnimationFrame", window.Get(vendor+"RequestAnimationFrame"))
+			window.Set("cancelAnimationFrame", window.Get(vendor+"CancelAnimationFrame"))
+			if window.Get("cancelAnimationFrame") == js.Undefined {
+				window.Set("cancelAnimationFrame", window.Get(vendor+"CancelRequestAnimationFrame"))
+			}
+		}
+	}
+
+	lastTime := 0.0
+	if window.Get("requestAnimationFrame") == js.Undefined {
+		window.Set("requestAnimationFrame", func(callback func(float32)) int {
+			currTime := js.Global.Get("Date").New().Call("getTime").Float()
+			timeToCall := math.Max(0, 16-(currTime-lastTime))
+			id := window.Call("setTimeout", func() { callback(float32(currTime + timeToCall)) }, timeToCall)
+			lastTime = currTime + timeToCall
+			return id.Int()
+		})
+	}
+
+	if window.Get("cancelAnimationFrame") == js.Undefined {
+		window.Set("cancelAnimationFrame", func(id int) {
+			js.Global.Get("clearTimeout").Invoke(id)
+		})
+	}
+}
+
+func RequestAnimationFrame(callback func(float32)) int {
+	return js.Global.Call("requestAnimationFrame", callback).Int()
+}
+
+func CancelAnimationFrame(id int) {
+	js.Global.Call("cancelAnimationFrame", id)
+}
