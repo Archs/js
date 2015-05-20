4e5c3ba83b491fc6e3e29c127365f29ad87dc6b2
Archs <gitarchs@yeah.net>
2015-05-21 01:39:29 +0800

    add contex2d.GetImage

--- gopherjs-ko/components/clock/clock.go
+++ gopherjs-ko/components/clock/clock.go
@@ -45,12 +45,28 @@ func registerClock() {
 				el := dom.Wrap(info.Get("element"))
 				el = el.QuerySelector("canvas")
 				c := canvas.New(el.Object)
-				c.Width = 20
-				c.Height = 20
+				c.Width = 200
+				c.Height = 200
 				ctx := c.GetContext2D()
-				ctx.FillStyle = "red"
-				ctx.FillRect(0, 0, 20, 20)
-				println(params, info, el, c, ctx)
+				img := dom.CreateElement("img")
+				img.Src = "https://mdn.mozillademos.org/files/222/Canvas_createpattern.png"
+				img.AddEventListener(dom.EvtLoad, func(e *dom.Event) {
+					p := ctx.Cp(img, "repeat")
+					ctx.FillStyle = p.Value()
+					ctx.FillRect(0, 0, 200, 200)
+					g := ctx.CreateRadialGradient(100, 100, 100, 100, 100, 0)
+					g.AddColorStop(0.0, "white")
+					g.AddColorStop(1.0, "green")
+					ctx.FillStyle = g.Value()
+					ctx.FillRect(0, 0, 200, 200)
+					println(params, info, el, c, ctx)
+				}, true)
+				// go func() {
+				// 	for {
+				// 		<-time.Tick(time.Second)
+				// 		println(time.Now())
+				// 	}
+				// }()
 			},
 		},
 	})
