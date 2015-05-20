4eeb874d7bb8f47cc7a5d100c248f1556ef9c7b5
Archs <gitarchs@yeah.net>
2015-05-20 17:09:02 +0800

    update js.canvas api

--- canvas/canvas.go
+++ canvas/canvas.go
@@ -1,7 +1,7 @@
 // Package canvas provides GopherJS bindings for the JavaScript canvas APIs.
 //
-// Code mainly borrowed from honnef.co/go/js/dom by Dominik Honnef in order to
-// be a separated wrapper of the JavaScript canvas API.
+// The code is mainly based on package honnef.co/go/js/dom by Dominik Honnef
+// in order to create a thin wrapper of the JavaScript canvas API.
 package canvas
 
 import (
@@ -9,11 +9,11 @@ import (
 	"github.com/gopherjs/gopherjs/js"
 )
 
-type CanvasElement struct {
-	dom.Element
+type Canvas struct {
+	*dom.Element
 }
 
-type CanvasRenderingContext2D struct {
+type Context2D struct {
 	*js.Object
 
 	// Colors, Styles, and Shadows
@@ -40,18 +40,19 @@ type CanvasRenderingContext2D struct {
 	GlobalCompositeOperation string  `js:"globalCompositeOperation"`
 }
 
-// el is then html element
-func NewElement(el *js.Object) *CanvasElement {
-	return &CanvasElement{dom.Element{Object: el}}
+// New creates a Canvas instance
+// el is the html element
+func New(el *js.Object) *Canvas {
+	return &Canvas{dom.Wrap(el)}
 }
 
-func (c *CanvasElement) GetContext2D() *CanvasRenderingContext2D {
+func (c *Canvas) GetContext2D() *Context2D {
 	ctx := c.Call("getContext", "2d")
-	return &CanvasRenderingContext2D{Object: ctx}
+	return &Context2D{Object: ctx}
 }
 
 // canvas.toDataURL("image/jpeg") or canvas.toDataURL()
-func (c *CanvasElement) ToDataUrl(mimeType ...string) string {
+func (c *Canvas) ToDataUrl(mimeType ...string) string {
 	var o *js.Object
 	if len(mimeType) == 0 {
 		o = c.Call("toDataURL")
@@ -63,103 +64,103 @@ func (c *CanvasElement) ToDataUrl(mimeType ...string) string {
 
 // Colors, Styles, and Shadows
 
-func (ctx *CanvasRenderingContext2D) CreateLinearGradient(x0, y0, x1, y1 int) {
+func (ctx *Context2D) CreateLinearGradient(x0, y0, x1, y1 int) {
 	ctx.Call("createLinearGradient", x0, y0, x1, y1)
 }
 
 // Rectangles
 
-func (ctx *CanvasRenderingContext2D) Rect(x, y, width, height int) {
+func (ctx *Context2D) Rect(x, y, width, height int) {
 	ctx.Call("rect", x, y, width, height)
 }
 
-func (ctx *CanvasRenderingContext2D) FillRect(x, y, width, height int) {
+func (ctx *Context2D) FillRect(x, y, width, height int) {
 	ctx.Call("fillRect", x, y, width, height)
 }
 
-func (ctx *CanvasRenderingContext2D) StrokeRect(x, y, width, height int) {
+func (ctx *Context2D) StrokeRect(x, y, width, height int) {
 	ctx.Call("strokeRect", x, y, width, height)
 }
 
-func (ctx *CanvasRenderingContext2D) ClearRect(x, y, width, height int) {
+func (ctx *Context2D) ClearRect(x, y, width, height int) {
 	ctx.Call("clearRect", x, y, width, height)
 }
 
 // Paths
 
-func (ctx *CanvasRenderingContext2D) Fill() {
+func (ctx *Context2D) Fill() {
 	ctx.Call("fill")
 }
 
-func (ctx *CanvasRenderingContext2D) Stroke() {
+func (ctx *Context2D) Stroke() {
 	ctx.Call("stroke")
 }
 
-func (ctx *CanvasRenderingContext2D) BeginPath() {
+func (ctx *Context2D) BeginPath() {
 	ctx.Call("beginPath")
 }
 
-func (ctx *CanvasRenderingContext2D) MoveTo(x, y int) {
+func (ctx *Context2D) MoveTo(x, y int) {
 	ctx.Call("moveTo", x, y)
 }
 
-func (ctx *CanvasRenderingContext2D) ClosePath() {
+func (ctx *Context2D) ClosePath() {
 	ctx.Call("closePath")
 }
 
-func (ctx *CanvasRenderingContext2D) LineTo(x, y int) {
+func (ctx *Context2D) LineTo(x, y int) {
 	ctx.Call("lineTo", x, y)
 }
 
-func (ctx *CanvasRenderingContext2D) Clip() {
+func (ctx *Context2D) Clip() {
 	ctx.Call("clip")
 }
 
-func (ctx *CanvasRenderingContext2D) QuadraticCurveTo(cpx, cpy, x, y int) {
+func (ctx *Context2D) QuadraticCurveTo(cpx, cpy, x, y int) {
 	ctx.Call("quadraticCurveTo", cpx, cpy, x, y)
 }
 
-func (ctx *CanvasRenderingContext2D) BezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y int) {
+func (ctx *Context2D) BezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y int) {
 	ctx.Call("bezierCurveTo", cp1x, cp1y, cp2x, cp2y, x, y)
 }
 
-func (ctx *CanvasRenderingContext2D) Arc(x, y, r int, sAngle, eAngle float32, counterclockwise bool) {
+func (ctx *Context2D) Arc(x, y, r int, sAngle, eAngle float32, counterclockwise bool) {
 	ctx.Call("arc", x, y, r, sAngle, eAngle, counterclockwise)
 }
 
-func (ctx *CanvasRenderingContext2D) ArcTo(x1, y1, x2, y2, r int) {
+func (ctx *Context2D) ArcTo(x1, y1, x2, y2, r int) {
 	ctx.Call("arcTo", x1, y1, x2, y2, r)
 }
 
-func (ctx *CanvasRenderingContext2D) IsPointInPath(x, y int) bool {
+func (ctx *Context2D) IsPointInPath(x, y int) bool {
 	return ctx.Call("isPointInPath", x, y).Bool()
 }
 
 // Transformations
 
-func (ctx *CanvasRenderingContext2D) Scale(scaleWidth, scaleHeight int) {
+func (ctx *Context2D) Scale(scaleWidth, scaleHeight int) {
 	ctx.Call("scale", scaleWidth, scaleHeight)
 }
 
-func (ctx *CanvasRenderingContext2D) Rotate(angle int) {
+func (ctx *Context2D) Rotate(angle int) {
 	ctx.Call("rotate", angle)
 }
 
-func (ctx *CanvasRenderingContext2D) Translate(x, y int) {
+func (ctx *Context2D) Translate(x, y int) {
 	ctx.Call("translate", x, y)
 }
 
-func (ctx *CanvasRenderingContext2D) Transform(a, b, c, d, e, f int) {
+func (ctx *Context2D) Transform(a, b, c, d, e, f int) {
 	ctx.Call("transform", a, b, c, d, e, f)
 }
 
-func (ctx *CanvasRenderingContext2D) SetTransform(a, b, c, d, e, f int) {
+func (ctx *Context2D) SetTransform(a, b, c, d, e, f int) {
 	ctx.Call("setTransform", a, b, c, d, e, f)
 }
 
 // Text
 
-func (ctx *CanvasRenderingContext2D) FillText(text string, x, y, maxWidth int) {
+func (ctx *Context2D) FillText(text string, x, y, maxWidth int) {
 	if maxWidth == -1 {
 		ctx.Call("fillText", text, x, y)
 		return
@@ -168,7 +169,7 @@ func (ctx *CanvasRenderingContext2D) FillText(text string, x, y, maxWidth int) {
 	ctx.Call("fillText", text, x, y, maxWidth)
 }
 
-func (ctx *CanvasRenderingContext2D) StrokeText(text string, x, y, maxWidth int) {
+func (ctx *Context2D) StrokeText(text string, x, y, maxWidth int) {
 	if maxWidth == -1 {
 		ctx.Call("strokeText", text, x, y)
 		return
@@ -179,10 +180,10 @@ func (ctx *CanvasRenderingContext2D) StrokeText(text string, x, y, maxWidth int)
 
 // canvas state
 
-func (ctx *CanvasRenderingContext2D) Save() {
+func (ctx *Context2D) Save() {
 	ctx.Call("save")
 }
 
-func (ctx *CanvasRenderingContext2D) Restore() {
+func (ctx *Context2D) Restore() {
 	ctx.Call("restore")
 }
