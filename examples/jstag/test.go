be4a48d5c100b3e262fa912ee04415d0953aeb81
Archs <gitarchs@yeah.net>
2015-05-22 21:05:08 +0800

    add example jstag

---
+++ examples/jstag/test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gopherjs/gopherjs/js"
+)
+
+type A struct {
+	*js.Object
+	Method func() `js:"Method"` // js tag with method invoke, panic
+	// Method func() // this is fine
+}
+
+func main() {
+	a := &A{
+		Object: js.Global.Get("Object").New(),
+		Method: func() {
+			println("hello")
+		},
+	}
+	println(a.Method) // this is fine
+	a.Method()        // this cause panic
+}
