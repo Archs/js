3513273d3b1e3cd5d4b54c26d33aeba5017718b3
Archs <sun@archs.tk>
2015-10-18 09:53:34 +0800

    using *.inc.js is not done yet

--- examples/ko-uploader/ko-uploader.go
+++ examples/ko-uploader/ko-uploader.go
@@ -4,9 +4,11 @@ import (
 	"github.com/Archs/js/dom"
 	"github.com/Archs/js/gopherjs-ko"
 	"github.com/Archs/js/gopherjs-ko/components/uploader"
+	"github.com/Archs/js/gopherjs-ko/plugins/csp"
 )
 
 func main() {
+	csp.EnableSecureBinding()
 	uploader.SetCallback(func(url string, files []*dom.File) {
 		for _, file := range files {
 			println("upload", file.Name, "to", url, "done")
