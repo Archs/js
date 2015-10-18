0157f1a87eedcaee93e943496fa3434513731e37
Archs <gitarchs@yeah.net>
2015-10-18 14:01:44 +0800

    update test

--- examples/ko-uploader/ko-uploader.go
+++ examples/ko-uploader/ko-uploader.go
@@ -4,18 +4,14 @@ import (
 	"github.com/Archs/js/dom"
 	"github.com/Archs/js/gopherjs-ko"
 	"github.com/Archs/js/gopherjs-ko/components/uploader"
-	"github.com/Archs/js/gopherjs-ko/plugins/async"
 )
 
 func main() {
-	dom.OnDOMContentLoaded(func() {
-		println(`ko.Get("utils"):`, ko.Get("utils"))
-		async.AsyncTemplateConfig().SetDefaultPath(".")
-		uploader.SetCallback(func(url string, files []*dom.File) {
-			for _, file := range files {
-				println("upload", file.Name, "to", url, "done")
-			}
-		})
-		ko.ApplyBindings(nil)
+	println(`ko.Get("utils"):`, ko.Get("utils"))
+	uploader.SetCallback(func(url string, files []*dom.File) {
+		for _, file := range files {
+			println("upload", file.Name, "to", url, "done")
+		}
 	})
+	ko.ApplyBindings(nil)
 }
--- examples/ko/main.go
+++ examples/ko/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/Archs/js/gopherjs-ko"
 	_ "github.com/Archs/js/gopherjs-ko/components/clock"
-	_ "github.com/Archs/js/gopherjs-ko/components/vCard"
 )
 
 func main() {
