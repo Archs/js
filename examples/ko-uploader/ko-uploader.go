bed3ff497a938dfb0c6b31f5d06ad789315f3e9b
Archs <sun@archs.tk>
2015-10-18 11:43:35 +0800

    update

--- examples/ko-uploader/ko-uploader.go
+++ examples/ko-uploader/ko-uploader.go
@@ -4,15 +4,18 @@ import (
 	"github.com/Archs/js/dom"
 	"github.com/Archs/js/gopherjs-ko"
 	"github.com/Archs/js/gopherjs-ko/components/uploader"
-	"github.com/Archs/js/gopherjs-ko/plugins/csp"
+	"github.com/Archs/js/gopherjs-ko/plugins/async"
 )
 
 func main() {
-	csp.EnableSecureBinding()
-	uploader.SetCallback(func(url string, files []*dom.File) {
-		for _, file := range files {
-			println("upload", file.Name, "to", url, "done")
-		}
+	dom.OnDOMContentLoaded(func() {
+		println(`ko.Get("utils"):`, ko.Get("utils"))
+		async.AsyncTemplateConfig().SetDefaultPath(".")
+		uploader.SetCallback(func(url string, files []*dom.File) {
+			for _, file := range files {
+				println("upload", file.Name, "to", url, "done")
+			}
+		})
+		ko.ApplyBindings(nil)
 	})
-	ko.ApplyBindings(nil)
 }
