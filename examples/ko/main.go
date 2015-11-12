b0e6c8445b07d5f9b152b8769cc3e2f5414d0870
Archs <gitarchs@yeah.net>
2015-11-12 15:50:45 +0100

    update

--- examples/ko/main.go
+++ examples/ko/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/Archs/js/gopherjs-ko"
 	_ "github.com/Archs/js/gopherjs-ko/components/clock"
+	_ "github.com/Archs/js/gopherjs-ko/components/vCard"
 )
 
 func main() {
